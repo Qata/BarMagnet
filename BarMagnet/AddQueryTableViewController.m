959a1673d53bbce92e4ade6a150dbcc2e0b99754
Qata Life <qatagee@gmail.com>
2014-02-03 11:09:27 +1100

    Changed the save button location.

--- BarMagnet/AddQueryTableViewController.m
+++ BarMagnet/AddQueryTableViewController.m
@@ -29,37 +29,33 @@
 	return [textField resignFirstResponder];
 }
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+- (IBAction)save:(id)sender
 {
-	[tableView deselectRowAtIndexPath:indexPath animated:YES];
-	if ([[tableView cellForRowAtIndexPath:indexPath].textLabel.text isEqualToString:@"Save"])
+	if (self.name.text.length && self.URL.text.length)
 	{
-		if (self.name.text.length && self.URL.text.length)
+		NSString * URL = [self.URL.text stringByReplacingOccurrencesOfString:@"http://" withString:@""];
+		if (!self.usesQuery.on || [URL rangeOfString:@"%query%"].location != NSNotFound)
 		{
-			NSString * URL = [self.URL.text stringByReplacingOccurrencesOfString:@"http://" withString:@""];
-			if (!self.usesQuery.on || [URL rangeOfString:@"%query%"].location != NSNotFound)
+			NSMutableArray * array = [[NSUserDefaults.standardUserDefaults objectForKey:@"queries"] mutableCopy];
+			if (!array)
 			{
-				NSMutableArray * array = [[NSUserDefaults.standardUserDefaults objectForKey:@"queries"] mutableCopy];
-				if (!array)
-				{
-					array = NSMutableArray.new;
-				}
-				NSDictionary * object = @{@"name":self.name.text, @"query":URL, @"uses_query":@(self.usesQuery.on)};
-				if (self.queryDictionary)
-				{
-					[array replaceObjectAtIndex:[[NSUserDefaults.standardUserDefaults objectForKey:@"queries"] indexOfObject:self.queryDictionary] withObject:object];
-				}
-				else
-				{
-					[array addObject:object];
-				}
-				[NSUserDefaults.standardUserDefaults setObject:array forKey:@"queries"];
-				[self dismiss:nil];
+				array = NSMutableArray.new;
+			}
+			NSDictionary * object = @{@"name":self.name.text, @"query":URL, @"uses_query":@(self.usesQuery.on)};
+			if (self.queryDictionary)
+			{
+				[array replaceObjectAtIndex:[[NSUserDefaults.standardUserDefaults objectForKey:@"queries"] indexOfObject:self.queryDictionary] withObject:object];
 			}
 			else
 			{
-				[[UIAlertView.alloc initWithTitle:@"Error" message:@"The query URL needs to have \"%query%\" in it where the search parameters usually go" delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles:nil] show];
+				[array addObject:object];
 			}
+			[NSUserDefaults.standardUserDefaults setObject:array forKey:@"queries"];
+			[self dismiss:nil];
+		}
+		else
+		{
+			[[UIAlertView.alloc initWithTitle:@"Error" message:@"The query URL needs to have \"%query%\" in it where the search parameters usually go" delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles:nil] show];
 		}
 	}
 }
