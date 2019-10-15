0f99ddfc5dea6365db617999490f52c1c27f77a1
Charlotte Tortorella <charlotte@monadic.consulting>
2019-10-15 20:57:49 +1100

    Update NSBencodeSerialization and build.sh to work with the latest Xcode

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xcodebuild -project BarMagnet.xcodeproj -scheme BarMagnet -sdk iphoneos -configuration Release CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+xcodebuild -project BarMagnet.xcodeproj -scheme BarMagnet -sdk iphoneos -configuration Release CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO
